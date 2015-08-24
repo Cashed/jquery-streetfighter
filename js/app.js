429efa3ff483885ea2e458fc0c272faf52258b5f
Cashed <frau.veronika@gmail.com>
2015-08-24 10:06:45 -0700

    added background and finished cool pose

--- js/app.js
+++ js/app.js
@@ -1,15 +1,21 @@
 $(document).ready(function() {
 	$('.ryu').mouseenter(function() {
 		$('.ryu-still').hide();
+		$('.ryu-cool').hide();
+		$('.ryu-throwing').hide();
 		$('.ryu-ready').show();
 	})
 	.mouseleave(function() {
 		$('.ryu-ready').hide();
+		$('.ryu-throwing').hide();
+		$('.ryu-cool').hide();
 		$('.ryu-still').show();
 	})
 	.mousedown(function() {
 		playHadouken();
 		$('.ryu-ready').hide();
+		$('.ryu-still').hide();
+		$('.ryu-cool').hide();
 		$('.ryu-throwing').show();
 		$('.hadouken').finish().show().animate(
 			{'left': '1020px'},
@@ -24,6 +30,21 @@ $(document).ready(function() {
 		$('.ryu-throwing').hide();
 		$('.ryu-ready').show();
 	});
+
+})
+.keydown(function(e) {
+	if(e.keyCode == 88){
+		$('.ryu-ready').hide();
+		$('.ryu-still').hide();
+		$('.ryu-throwing').hide();
+		$('.ryu-cool').show();
+	}
+})
+.keyup(function(e){
+	if(e.keyCode == 88){
+		$('.ryu-cool').hide();
+		$('.ryu-still').show();
+	}
 });
 
 function playHadouken () {
